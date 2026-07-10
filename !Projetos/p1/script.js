42e349821cc4f0294fd25c269c1151d9a9f14515
Gabriel Brito <gabrielchillingbig@gmail.com>
2026-07-10 10:41:21 -0300

    UC15

--- !Projetos/p1/script.js
+++ !Projetos/p1/script.js
@@ -1,13 +1,13 @@
 const botao = document.getElementById("desc-btn");
 const descricao = document.getElementById("descricao");
-const arrowImage = document.querySelector("#desc-btn img");
+const flechinha = document.querySelector("#desc-btn img");
 
 botao.addEventListener("click", function () {
     if (descricao.style.display === "block") {
         descricao.style.display = "none";
-        arrowImage.classList.remove("rotated");
+        flechinha.classList.remove("rotated");
     } else {
         descricao.style.display = "block";
-        arrowImage.classList.add("rotated");
+        flechinha.classList.add("rotated");
     }
 });
