54272b4ff1b29ce702b5c936b31eea8df32fc712
Gabriel Brito <gabrielchillingbig@gmail.com>
2026-07-17 22:09:01 -0300

    UC15

--- !Projetos/Projeto AirPods Pro 3/script.js
+++ !Projetos/Projeto AirPods Pro 3/script.js
@@ -1,9 +1,18 @@
-window.addEventListener('scroll', () => {
-  const header = document.querySelector('.header-fixo');
+const gatilho = document.querySelector('#gatilho-scroll');
+const header = document.querySelector('.header-fixo');
 
-  if (window.scrollY >950) {
-    header.classList.add('colado');
-  } else {
-    header.classList.remove('colado');
-  }
+const observer = new IntersectionObserver((entries) => {
+  entries.forEach(entry => {
+    // Se o elemento gatilho NÃO estiver visível na tela (usuário rolou para baixo)
+    if (!entry.isIntersecting) {
+      header.classList.add('colado');
+    } else {
+      // Se o usuário voltou totalmente para o topo
+      header.classList.remove('colado');
+    }
+  });
+}, {
+  rootMargin: '0px 0px 0px 0px' /* Ajuste este valor para controlar a altura do disparo */
 });
+
+observer.observe(gatilho);
