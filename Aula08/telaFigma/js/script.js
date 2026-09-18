11565c459c0dce2c1c6eed4b97afb4efddd2b8a4
Gabriel Brito <gabrielchillingbig@gmail.com>
2026-09-18 11:50:11 -0300

    UC15

--- Aula08/telaFigma/js/script.js
+++ Aula08/telaFigma/js/script.js
@@ -4,5 +4,37 @@ function validar () {
     var descricao = document.getElementById("descricao").value;
     var autor = document.getElementById("autor").value;
 
-    alert ("Cadastro Confirmado com sucesso!\n\nTítulo: "+ titulo +"\nQuantidade de episódios: " + episodio + "\nDescricao: " + descricao +  "\nAutor: " + autor)
+    if (titulo && episodio && descricao && autor) {
+        localStorage.setItem("anime_titulo", titulo);
+        localStorage.setItem("anime_episodio", episodio);
+        localStorage.setItem("anime_descricao", descricao);
+        localStorage.setItem("anime_autor", autor);
+
+        window.location.href = "cadastroConfirmacao.html";
+    } else {
+        alert("Por Favor preencha todos os campos!");
+    }
 }
+
+function voltar() {
+    window.location.href = "index.html";
+}
+
+function confirmar() {
+    window.location.href = "cadastroConcluido.html";
+    localStorage.clear();
+}
+
+// Aguarda o HTML da página carregar por completo antes de executar o código interno
+window.addEventListener("DOMContentLoaded", function() {
+    // 1. Busca o primeiro elemento para testar se estamos na tela de confirmação
+    var pTitulo = document.getElementById("conf-titulo");
+
+    // 2. Só coloca os textos se o elemento realmente existir na página atual
+    if (pTitulo) {
+        pTitulo.textContent = localStorage.getItem("anime_titulo");
+        document.getElementById("conf-episodio").textContent = localStorage.getItem("anime_episodio");
+        document.getElementById("conf-descricao").textContent = localStorage.getItem("anime_descricao");
+        document.getElementById("conf-autor").textContent = localStorage.getItem("anime_autor");
+    }
+});
